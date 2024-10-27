64e33c612ad84198ba4623ee3daebd21248d23b2
Kevin Reyes <kerpa9@gmail.com>
2024-10-27 13:15:15 -0500

    Create all models

--- src/com/kevenreyes/screenmatch/models/Episode.java
+++ src/com/kevenreyes/screenmatch/models/Episode.java
@@ -1,5 +1,58 @@
 package com.kevenreyes.screenmatch.models;
 
-public class Episode {
-    
+import com.kevenreyes.screenmatch.calculus.IClasification;
+
+public class Episode implements IClasification {
+    private int number;
+    private String name;
+    private Series serie;
+    private int totalViews;
+
+    // Gettes
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Series getSerie() {
+        return serie;
+    }
+
+    public int getTotalViews() {
+        return totalViews;
+    }
+
+    // Settes
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSerie(Series serie) {
+        this.serie = serie;
+    }
+
+    public void setTotalViews(int totalViews) {
+        this.totalViews = totalViews;
+    }
+
+    // Methods
+    @Override
+    public int getClasification() {
+
+        if (totalViews > 100) {
+            return 4;
+        } else {
+            return 2;
+        }
+    }
+
 }
--- src/com/kevenreyes/screenmatch/models/Movie.java
+++ src/com/kevenreyes/screenmatch/models/Movie.java
@@ -1,5 +1,24 @@
 package com.kevenreyes.screenmatch.models;
 
-public class Movie {
-    
+import com.kevenreyes.screenmatch.calculus.IClasification;
+
+public class Movie extends Title implements IClasification {
+    private String director;
+
+    // Getter
+    public String getDirector() {
+        return director;
+    }
+
+    // Setter
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    @Override
+    public int getClasification() {
+        return (int) calMediaEvaluate() / 2;
+    }
+
 }
